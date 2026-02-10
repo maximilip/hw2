eff584736d11d64c1f828ccb8795c11bad041562
maximilip <max.prasad@kellogg.northwestern.edu>
2026-02-10 20:44:54 +0000

    migrate-studio-movies-table

---
+++ filmdomain.rb
@@ -0,0 +1,76 @@
+# Movies
+# ======
+# Batman Begins          2005           PG-13  Warner Bros.
+# The Dark Knight        2008           PG-13  Warner Bros.
+# The Dark Knight Rises  2012           PG-13  Warner Bros.
+
+# Top Cast
+# ========
+# Batman Begins          Christian Bale        Bruce Wayne
+# Batman Begins          Michael Caine         Alfred
+# Batman Begins          Liam Neeson           Ra's Al Ghul
+# Batman Begins          Katie Holmes          Rachel Dawes
+# Batman Begins          Gary Oldman           Commissioner Gordon
+# The Dark Knight        Christian Bale        Bruce Wayne
+# The Dark Knight        Heath Ledger          Joker
+# The Dark Knight        Aaron Eckhart         Harvey Dent
+# The Dark Knight        Michael Caine         Alfred
+# The Dark Knight        Maggie Gyllenhaal     Rachel Dawes
+# The Dark Knight Rises  Christian Bale        Bruce Wayne
+# The Dark Knight Rises  Gary Oldman           Commissioner Gordon
+# The Dark Knight Rises  Tom Hardy             Bane
+# The Dark Knight Rises  Joseph Gordon-Levitt  John Blake
+# The Dark Knight Rises  Anne Hathaway         Selina Kyle
+
+# Represented by agent
+# ====================
+# Christian Bale
+
+# Delete existing data, so you'll start fresh each time this script is run.
+# Use `Model.destroy_all` code.
+# TODO!
+# 
+# Model.destroy_all
+
+
+
+# Generate models and tables, according to the domain model.
+# TODO!
+# 
+# rails generate model Studio
+# rails generate model Movie
+# rails generate model Actor
+# rails generate model Role
+# rails generate model Agent
+# rails db:migrate
+# 
+#
+#Insert data into the database that reflects the sample data shown above.
+# Do not use hard-coded foreign key IDs.
+# TODO!
+
+# Prints a header for the movies output
+puts "Movies"
+puts "======"
+puts ""
+
+# Query the movies data and loop through the results to display the movies output.
+# TODO!
+
+# Prints a header for the cast output
+puts ""
+puts "Top Cast"
+puts "========"
+puts ""
+
+# Query the cast data and loop through the results to display the cast output for each movie.
+# TODO!
+
+# Prints a header for the agent's list of represented actors output
+puts ""
+puts "Represented by agent"
+puts "===================="
+puts ""
+
+# Query the actor data and loop through the results to display the agent's list of represented actors output.
+# TODO!
